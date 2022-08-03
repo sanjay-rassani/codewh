a2f1975aa67ae963be2b94cc4a92aa040a54a12e
Sanjay Kumar Rassani <67832091+isanjayrassani@users.noreply.github.com>
2022-08-03 09:40:49 +0500

    13 charcounter

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -3,6 +3,7 @@ from ssl import Purpose
 from string import punctuation
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.test import TestCase
 
 # def cont_disp(request):
 #     file = open('one.txt','r')
@@ -14,15 +15,20 @@ def index(request):
     return HttpResponse('<h1>Home</h1> <h2><a href="http://127.0.0.1:8000/rempunc">Remove Punctuation</a> | <a href="http://127.0.0.1:8000/capfirst">Capitalize First</a> | <a href="http://127.0.0.1:8000/nlrem">Newline Remove</a> | <a href="http://127.0.0.1:8000/sprem">Space Remover</a> | <a href="http://127.0.0.1:8000/charcount">Character Counter</a></h2>')
 
 def analyze(request):
+    # Get the text
     text = request.GET.get('text', 'default')
+
+    # Check checkbox values
     removepunc = request.GET.get('removepunc', 'off')
     fullcaps = request.GET.get('fullcaps', 'off')
-    print(removepunc)
-    print(text)
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+    charcounter = request.GET.get('charcounter', 'off')
     
+    # Check with checkbox is on 
     if removepunc == 'on':
-        analyzed = ''
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ''
         for char in text:
             if char not in punctuations:
                 analyzed = analyzed + char
@@ -36,6 +42,33 @@ def analyze(request):
 
         params = {'purpose': 'Changed to UPPERCASE', 'analyzed_text': analyzed}
         return render(request, 'textutils/analyze.html', params)
+
+    elif(newlineremover == 'on'):
+        analyzed = ''
+        for char in text:
+            if char != '\n':
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Newlines removed', 'analyzed_text': analyzed}
+        return render(request, 'textutils/analyze.html', params)
+
+    elif(extraspaceremover == 'on'):
+        analyzed = ''
+        for index, char in enumerate(text):
+            if text[index] == ' ' and text[index+1] == ' ':
+                pass
+            else:
+                analyzed = analyzed +char
+
+        params = {'purpose': 'Extra Space Remover', 'analyzed_text': analyzed}
+        return render(request, 'textutils/analyze.html', params)
+
+    elif(charcounter =='on'):
+        analyzed = str(len(text))
+
+        params = {'purpose': 'Counted the chars', 'analyzed_text': analyzed}
+        return render(request, 'textutils/analyze.html', params)
+
 
     else:
         return HttpResponse('Error')
