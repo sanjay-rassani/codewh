110309a92786c9dc8fd9755dedf2fa67579f5c9d
Sanjay Kumar Rassani <67832091+isanjayrassani@users.noreply.github.com>
2022-08-15 01:46:40 +0500

    updated thirteeth

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -6,5 +6,11 @@ class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=64)
     desc = models.CharField(max_length=300)
+    category = models.CharField(max_length=50, default='')
+    subcategory = models.CharField(max_length=50, default='')
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='shop/images', default='')
     pub_date = models.DateField()
-    
+
+    def __str__(self):
+        return f'{self.product_name}'
